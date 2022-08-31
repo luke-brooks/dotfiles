2778c8f856717d2f6563516db13c24c29916cf14
Luke Brooks <luke.brooks@kitcheck.com>
2022-08-31 15:43:41 -0400

    bash - activate auto nvm use

--- bash/construction-zone.sh
+++ bash/construction-zone.sh
@@ -7,17 +7,17 @@
 # a .nvmrc file in the directory. Also, revert to default
 # version when entering a directory without .nvmrc
 #
-# enter_directory() {
-# if [[ $PWD == $PREV_PWD ]]; then
-#  return
-# fi
-# PREV_PWD=$PWD
-# if [[ -f ".nvmrc" ]]; then
-#    nvm use
-#    NVM_DIRTY=true
-# elif [[ $NVM_DIRTY = true ]]; then # this may be broken...
-#    nvm use default
-#    NVM_DIRTY=false
-# fi
-# }
-# export PROMPT_COMMAND=enter_directory
+enter_directory() {
+if [[ $PWD == $PREV_PWD ]]; then
+ return
+fi
+PREV_PWD=$PWD
+if [[ -f ".nvmrc" ]]; then
+   nvm use
+   NVM_DIRTY=true
+elif [[ $NVM_DIRTY = true ]]; then # this may be broken...
+   nvm use default
+   NVM_DIRTY=false
+fi
+}
+export PROMPT_COMMAND=enter_directory
