7052dcfbc8ca3beffc5c915ecc6ec86402d8a88b
Luke Brooks <luke.brooks@kitcheck.com>
2022-09-06 09:39:14 -0400

    hs - use opera for lofi, remove sublime hotkey, format app-mgmt file

--- hs/app-manager.lua
+++ hs/app-manager.lua
@@ -1,39 +1,41 @@
-
-local function setAppManagerHotkeys (hotkeys, modifier)
-  for key, app in pairs(hotkeys) do
-    hs.hotkey.bind(modifier, key, function()
-      hs.application.launchOrFocus(app)
-      sleep(0.1)
-      appInstance = hs.application.find(app)
-      if (appInstance ~= nil) then
-        centerMouseCursor(appInstance:focusedWindow():screen(), appInstance:focusedWindow())
-      end
-    end)
-  end
+local function setAppManagerHotkeys(hotkeys, modifier)
+    for key, app in pairs(hotkeys) do
+        hs.hotkey.bind(modifier, key, function()
+            hs.application.launchOrFocus(app)
+            sleep(0.1)
+            appInstance = hs.application.find(app)
+            if (appInstance ~= nil) then
+                centerMouseCursor(appInstance:focusedWindow():screen(), appInstance:focusedWindow())
+            end
+        end)
+    end
 end
 
 hs.hotkey.bind(HYPER, 'space', function()
-  hs.reload()
+    hs.reload()
 end)
 
-hs.notify.new({title='Hammerspoon', informativeText='Config loaded breh'}):send()
+hs.notify.new({
+    title = 'Hammerspoon',
+    informativeText = 'Config loaded breh'
+}):send()
 
 -- app manager
 local appHyperKeys = {
-  a = 'Slack',
-  s = 'Gmail',
-  d = 'Google Calendar',
-  f = 'Sublime Text',
-  x = 'iTerm',
-  c = 'Google Chrome',
-  v = 'Code',
-  u = 'Spotify'
+    a = 'Slack',
+    s = 'Gmail',
+    d = 'Google Calendar',
+    -- f = 'Sublime Text',
+    x = 'iTerm',
+    c = 'Google Chrome',
+    v = 'Code',
+    u = 'Spotify'
 }
 setAppManagerHotkeys(appHyperKeys, HYPER)
 
 local appMehKeys = {
-  e = 'Evernote',
-  r = 'Google Meet/HangOut' -- i literally never use this
+    e = 'Evernote',
+    r = 'Google Meet/HangOut' -- i literally never use this
 }
 setAppManagerHotkeys(appMehKeys, MEH)
 
@@ -65,17 +67,20 @@ hs.hotkey.bind(MEH, '7', function()
 end)
 
 -- also used in monitor-manager.lua
-function openLofiHipHopChrome ()
+function openLofiHipHopChrome()
     -- chrome = hs.application.find('Brave') -- i want to switch to a non-chrome browser to avoid this tab getting in the way
-    chrome = hs.application.find('Chrome')
-    lofi = chrome:findWindow('lofi hip hop')
-    if (lofi == nil) then
-        chrome:selectMenuItem({'File', 'New Window'})
-        sleep(0.5) -- wait for new window
-        lofi = chrome:findWindow('New Tab')
-        lofi:focus()
-        hs.eventtap.keyStrokes(LOFI_HIPHOP_YOUTUBE_URL)
-        hs.eventtap.keyStroke({}, 'return')
-    end
+    -- chrome = hs.application.find('Chrome')
+    opera = hs.application.open('Opera', 1)
+    lofi = opera:findWindow('lofi hip hop')
+    -- if (lofi == nil) then
+    --     -- opera:selectMenuItem({'File', 'New Window'})
+    --     -- chrome:selectMenuItem({'File', 'New Window'})
+    --     -- sleep(1) -- wait for new window
+    --     -- lofi = chrome:findWindow('New Tab') 
+    --     lofi = opera:findWindow('lofi hip hop')
+    --     -- lofi:focus()
+    --     -- hs.eventtap.keyStrokes('https://www.youtube.com/watch?v=jfKfPfyJRdk')
+    --     -- hs.eventtap.keyStroke({}, 'return')
+    -- end
     return lofi
 end
