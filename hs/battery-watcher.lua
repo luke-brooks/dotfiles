ca77c746b21302bbf7d1dd1fd35aaaf4d33528f8
Luke Brooks <luke.brooks@kitcheck.com>
2022-09-07 19:23:01 -0400

    hs - update battery watcher to no longer use amperage

--- hs/battery-watcher.lua
+++ hs/battery-watcher.lua
@@ -5,8 +5,13 @@ local chargingingIcon = nil
 function batteryUsage ()
     -- TODO: show error when power is below 15%
     if (hs.battery.percentage() < 70) then
-        muhAmps = hs.battery.amperage()
-        if (muhAmps < 0) then
+        -- muhAmps = hs.battery.amperage() -- amperage only returns nil with the m1....
+        -- if (muhAmps < 0) then
+        isCharging = hs.battery.isCharging() -- this is less reliable
+        -- its possible to be "charging" while burning more power than is incoming 
+        -- causing a decline in battery charge 
+        -- which is why amperage was more reliable
+        if (isCharging < 0) then
             chargingingIcon = removeIcon(chargingingIcon)
             drainingIcon = createIcon(drainingIcon, DRAINING_BATTERY_ICON, 'LOSIN POWAH')
         else
