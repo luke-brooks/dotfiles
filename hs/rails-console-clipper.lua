fa62c5718dd7b28aaccbdb5b93e02fe0da9d2c23
Luke Brooks <luke.brooks@kitcheck.com>
2022-10-03 16:21:15 -0400

    hs - rails console clipper skip null payloads

--- hs/rails-console-clipper.lua
+++ hs/rails-console-clipper.lua
@@ -9,36 +9,40 @@ local skipPayloadEdit = false;
 ----------------------------
 local function handlePaste(payload)
     -- print('new paste payload ' .. payload)
-    if (not skipPayloadEdit) then
-        local lines = {}
-        local firstLine = true
-        for str in payload:gmatch("[^\r\n]+") do
-            local result, count = string.gsub(str, irbLinePattern, "")
-            if (count > 0) then
-                if (result:len() > 0) then result = result:sub(2) end -- remove first space if not blank line
-                -- print('saving ruby snippet ' .. result)
-                table.insert(lines, result)
-            elseif (not firstLine) then
-                -- print('saving comment ' .. '# ' .. str)
-                table.insert(lines, '# ' .. str)
-            end
+    if (payload ~= nil) then
+        if (not skipPayloadEdit) then
+            local lines = {}
+            local firstLine = true
+            for str in payload:gmatch("[^\r\n]+") do
+                local result, count = string.gsub(str, irbLinePattern, "")
+                if (count > 0) then
+                    if (result:len() > 0) then
+                        result = result:sub(2)
+                    end -- remove first space if not blank line
+                    -- print('saving ruby snippet ' .. result)
+                    table.insert(lines, result)
+                elseif (not firstLine) then
+                    -- print('saving comment ' .. '# ' .. str)
+                    table.insert(lines, '# ' .. str)
+                end
 
-            -- print('lines table length' .. tableLength(lines))
-            if (firstLine and tableLength(lines) == 0) then
-                -- print('breaking loop due to no irb in first line')
-                -- end loop because first line wasnt from irb
-                break
+                -- print('lines table length' .. tableLength(lines))
+                if (firstLine and tableLength(lines) == 0) then
+                    -- print('breaking loop due to no irb in first line')
+                    -- end loop because first line wasnt from irb
+                    break
+                end
+                firstLine = false
             end
-            firstLine = false
+            if (tableLength(lines) > 0) then
+                skipPayloadEdit = true -- skip self-triggered event created by updating pasteboard
+                local newPayload = tableToString(lines)
+                -- print('new payload ' .. newPayload)
+                hs.pasteboard.setContents(newPayload)
+            end
+        else
+            skipPayloadEdit = false
         end
-        if (tableLength(lines) > 0) then
-            skipPayloadEdit = true -- skip self-triggered event created by updating pasteboard
-            local newPayload = tableToString(lines)
-            -- print('new payload ' .. newPayload)
-            hs.pasteboard.setContents(newPayload)
-        end
-    else
-        skipPayloadEdit = false
     end
 end
 
