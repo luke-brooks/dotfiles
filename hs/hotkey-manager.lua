ec61a822e5605124a88d770cc785f1e01bfacf8e
Luke Brooks <luke.brooks@kitcheck.com>
2022-10-03 16:20:20 -0400

    hs - refactor hotkeys into iterable list

--- hs/hotkey-manager.lua
+++ hs/hotkey-manager.lua
@@ -1,55 +1,60 @@
+local function customBindHotkeys(hotKeyTable)
+    printTable(hotKeyTable)
+    for index, mapping in pairs(hotKeyTable) do
+        -- print('examining ' .. index .. ' and ' .. tableToString(mapping))
+        hs.hotkey.bind(mapping[1], mapping[2], mapping[3])
+    end
+end
 
-----------------------------
--- System Controls
-----------------------------
-hs.hotkey.bind(HYPER, 'space', system_ReloadHsConfig)
-hs.hotkey.bind(MEH, '6', system_LockScreen)
+allHotkeys = {
+    ----------------------------
+    -- System Controls
+    ----------------------------
+    {HYPER, 'space', system_ReloadHsConfig},
+    {MEH, '6', system_LockScreen},
 
+    ----------------------------
+    -- Temporary App Hotkey
+    ----------------------------
+    {HYPER, '6', tempApp_SetKeyBinding},
+    {HYPER, tempKey, temp_AppFocus},
 
-----------------------------
--- Temporary App Hotkey
-----------------------------
-hs.hotkey.bind(HYPER, '6', tempApp_SetKeyBinding)
-hs.hotkey.bind(HYPER, tempKey, temp_AppFocus)
+    ----------------------------
+    -- Window Controls
+    ----------------------------
+    -- Left Hand 
+    {HYPER, 'home', window_FullScreen},
+    {HYPER, 'up', window_MoveTo_Previous_Screen},
+    {HYPER, 'down', window_MoveTo_Next_Screen},
 
+    -- Right Hand
+    {HYPER, 'h', window_LeftHalfSnap},
+    {HYPER, 'j', window_FullScreen},
+    {HYPER, 'k', window_MoveTo_Next_Screen},
+    {HYPER, 'l', window_RightHalfSnap},
 
-----------------------------
--- Window Controls
-----------------------------
+    ----------------------------
+    -- Vertical Monitor Grid Controls
+    ----------------------------
+    {MEH, '1', vertMonitorGrid_TopAndMid},
+    {MEH, '2', vertMonitorGrid_Bottom},
+    {MEH, '3', vertMonitorGrid_Middle},
+    {MEH, '4', vertMonitorGrid_Top},
 
--- Left Hand 
-hs.hotkey.bind(HYPER, 'home', window_FullScreen)
-hs.hotkey.bind(HYPER, 'up', window_MoveTo_Previous_Screen)
-hs.hotkey.bind(HYPER, 'down', window_MoveTo_Next_Screen)
+    ----------------------------
+    -- Zoom Controls
+    ----------------------------
+    {MEH, '8', zoom_FocusWindow},
+    {MEH, '9', zoom_ToggleAudioMute},
+    {MEH, '0', zoom_ToggleVideoMute},
+    {MEH, 'y', zoom_LeaveMeeting},
+    {MEH, 'u', zoom_OpenChat},
+    {MEH, 'i', zoom_ShowShareControls},
 
--- Right Hand
-hs.hotkey.bind(HYPER, 'h', window_LeftHalfSnap)
-hs.hotkey.bind(HYPER, 'j', window_FullScreen)
-hs.hotkey.bind(HYPER, 'k', window_MoveTo_Next_Screen)
-hs.hotkey.bind(HYPER, 'l', window_RightHalfSnap)
+    ----------------------------
+    -- Miscellaneous Controls
+    ----------------------------
+    {HYPER, 'y', misc_emojiAsciiArt},
+}
 
-
-----------------------------
--- Vertical Monitor Grid Controls
-----------------------------
-hs.hotkey.bind(MEH, '1', vertMonitorGrid_TopAndMid)
-hs.hotkey.bind(MEH, '2', vertMonitorGrid_Bottom)
-hs.hotkey.bind(MEH, '3', vertMonitorGrid_Middle)
-hs.hotkey.bind(MEH, '4', vertMonitorGrid_Top)
-
-
-----------------------------
--- Zoom Controls
-----------------------------
-hs.hotkey.bind(MEH, '8', zoom_FocusWindow)
-hs.hotkey.bind(MEH, '9', zoom_ToggleAudioMute)
-hs.hotkey.bind(MEH, '0', zoom_ToggleVideoMute)
-hs.hotkey.bind(MEH, 'y', zoom_LeaveMeeting)
-hs.hotkey.bind(MEH, 'u', zoom_OpenChat)
-hs.hotkey.bind(MEH, 'i', zoom_ShowShareControls)
-
-
-----------------------------
--- Miscellaneous Controls
-----------------------------
-hs.hotkey.bind(HYPER, 'y', misc_emojiAsciiArt)
+customBindHotkeys(allHotkeys)
