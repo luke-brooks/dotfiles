075118445ed907353bf6322ca23c4b0a514de086
Luke Brooks <luke.brooks@kitcheck.com>
2023-07-03 17:24:28 -0400

    add randomized quotes to slack status updater

--- python/slack-update-profile-status.py
+++ python/slack-update-profile-status.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import random
 
 url = 'https://bluesight.slack.com/api/users.profile.set'
 
@@ -9,12 +10,19 @@ mySuperSecretCookie = 'hands-off-my-cookies'
 
 expiration = time.time() + 300 # add 5 minutes to life of status, ends up closer to 3 minutes when accounting for deliver & processing delays
 
-print('expiration')
-print(expiration)
-
 headers = {'cookie': mySuperSecretCookie}
 
-profile = { "status_emoji": ":blunt_parrot:", "status_expiration": expiration, "status_text": "xXx420xBLAZExITxXx", "status_text_canonical": "" }
+quotes = [
+    "xxx420xBLAZExITxxx", # every 13yo's gamertag in 2008
+    "Dave's not here, man.", # cheech and chong
+    "Drugs are bad mmkay.", # south park
+    "This is, like, the apex of the vortex of joint engineering.", # pineapple express
+    "If you were yogurt, would you be fruit at the bottom or stirred?", # bio-dome
+]
+rand_int = random.randint(0, len(quotes))
+random_quote = quotes[rand_int]
+
+profile = { "status_emoji": ":blunt_parrot:", "status_expiration": expiration, "status_text": random_quote, "status_text_canonical": "" }
 profileJson = json.dumps(profile)
 
 # https://stackoverflow.com/a/12385661/8677309
@@ -32,3 +40,6 @@ response = session.post(url, headers=headers, files=(('profile', (None, profileJ
 # desired schedules:
 # (18 4 * * *)
 # (18 16 * * *)
+
+# crontab config
+# 18 16 * * * /usr/bin/python3 /Users/LukeBrooks/code/learning-repos/dotfiles/python/slack-update-profile-status.py 
